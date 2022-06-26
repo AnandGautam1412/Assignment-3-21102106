1669f8144e5828a35d10458537da26307a8b74f1
AnandGautam1412 <104383379+AnandGautam1412@users.noreply.github.com>
2022-06-27 00:53:19 +0530

    Add files via upload

---
+++ assign 3 output.py
@@ -0,0 +1,55 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====== RESTART: C:/Users/asus/OneDrive/Desktop/New folder/assignment 3.py ======
+Q1
+Enter a number50
+Binary equivalent of 50 is 110010
+Q2
+Enter expression:98/12
+98/12 = 8.166666666666666
+Q3
+Enter an integer6
+a) 104
+b) 15.0
+enter radius8
+c) 804.247719318987
+Enter angle 1:35
+Enter angle 1:96
+d) 3.7779406607428525
+Enter x coordinate of first point:52
+Enter y coordinate of first point:4
+Enter x coordinate of second point:55
+Enter y coordinate of second point:4
+e) 0.0
+Q4
+4.(a)
+0
+1
+2
+3
+4
+4.(b)
+3
+4
+5
+6
+7
+8
+9
+4.(c)
+4
+7
+10
+4.(d)
+15
+13
+11
+9
+7
+4.(e)
+Q5
+Enter number of carbon atoms in molecule:2
+Enter number of hydrogen atoms in molecule:4
+Enter number of oxygen atoms in molecule:1
+The molecular weight of the compound is 44.05256
